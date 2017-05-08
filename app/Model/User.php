dd69dd03214eed5b4a759a955f0ee7bd2113cdf5
raul suarez <raul.suarez12@gmail.com>
2017-05-07 20:12:22 -0430

    se crea el modulo usuarios para montar el Backend

---
+++ app/Model/User.php
@@ -0,0 +1,64 @@
+<?php
+App::uses('AppModel', 'Model');
+App::uses('BlowfishPasswordHasher', 'Controller/Component/Auth');
+/**
+ * User Model
+ *
+ */
+class User extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'username';
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'username' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				'message' => 'Ingrese su Usuario',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'password' => array(
+			'notEmpty' => array(
+				'rule' => 'alphanumeric',//notBlank
+				'message' => 'El Password es Requerido',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'role' => array(
+			'notEmpty' => array(
+				'rule' => array('inList', array('admin', 'secretario')),
+				'message' => 'Seleccione tipo de Usuario',
+				'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+
+	public function beforeSave($options = array()) {
+	    if (isset($this->data[$this->alias]['password'])) {
+	        $passwordHasher = new BlowfishPasswordHasher();
+	        $this->data[$this->alias]['password'] = $passwordHasher->hash(
+	            $this->data[$this->alias]['password']
+	        );
+	    }
+	    return true;
+	}
+}
